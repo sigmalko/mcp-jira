0f759816b614605c9dd1ee00d0e9ec201244725a
sigmalko <github@lingmine.pl>
2026-08-25 07:55:06 +0200

    start MCP Server in bash and docker

---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+docker pull ghcr.io/sooperset/mcp-atlassian:latest
+docker run -d --name mcp-jira --restart unless-stopped -p 127.0.0.1:9000:9000 --env-file .env ghcr.io/sooperset/mcp-atlassian:latest --transport streamable-http --stateless --port 9000
+# See MCP server at http://127.0.0.1:9000/mcp
